79ddb5c6942e053f6aa3460b18a53066f1d3fc8b
ValenciaTravis <jackshliu@sjtu.edu.cn>
2025-12-24 03:02:42 +0800

    finish exercise 2.1 (a)

--- utils.py
+++ utils.py
@@ -3,13 +3,14 @@ import pandas as pd
 def data_preprocess(input_csv, output_csv, start_date, end_date):
     df = pd.read_csv(input_csv)  
     df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
-    start_date_dt = pd.to_
+    start_date_dt = pd.to_datetime(start_date)
     end_date_dt = pd.to_datetime(end_date)
-    df = df[df['date'] <= end_date]
+    df = df[start_date_dt <= df['date']]
+    df = df[df['date'] <= end_date_dt]
+    df['date_time'] = pd.to_datetime(df['date'].dt.strftime('%Y-%m-%d') + ' ' + df['time'])
+    df = df[['date_time', 'price', 'volume']]
     df.to_csv(output_csv, index=False)
 
-def load_data():
-    df = pd.read_csv("data/raw.csv")
-    df['date_time'] = pd.to_datetime(df['date'] + ' ' + df['time'])
-    df = df[['date_time', 'price', 'volume']]
+def load_data(input_csv):
+    df = pd.read_csv(input_csv)
     return df
